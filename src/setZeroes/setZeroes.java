207c5cc1c647abb8dd36c34e504c50eb09e6a513
Ruby <gaojr0110@sina.com>
2018-03-19 21:19:20 +0800

    leetcode10

---
+++ src/setZeroes/setZeroes.java
@@ -0,0 +1,40 @@
+package setZeroes;
+
+class Solution{
+    public static void setZeroes(int[][] matrix){
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int col0 = 1;
+        for(int i = 0; i < rows; i ++) {
+            if(matrix[i][0] == 0)
+                col0 = 0;
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i = rows - 1; i >= 0; i --){
+            for(int j = cols - 1; j >= 1; j --){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+            if(col0 == 0)
+                matrix[i][0] = 0;
+        }
+    }
+}
+
+public class setZeroes {
+    public static void main(String[] args){
+        int[][] matrix = {{0, 1, 2}, {2, 1, 2}, {0, 5, 4}};
+        Solution.setZeroes(matrix);
+        for(int i = 0; i < matrix.length; i ++) {
+            for (int j = 0; j < matrix.length; j++)
+                System.out.print(matrix[i][j] + "\t");
+            System.out.println();
+        }
+    }
+}
