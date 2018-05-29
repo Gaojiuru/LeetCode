cb57bd165b5d29ffbdc5ebe351866a507c7ffb15
Ruby <gaojr0110@sina.com>
2018-05-29 16:27:29 +0800

    87

---
+++ src/isScramble/IsScramble.java
@@ -0,0 +1,33 @@
+package isScramble;
+
+class Solution{
+    public boolean isScramble(String s1, String s2){
+        if(s1.equals(s2))
+            return true;
+        int[] letter = new int[26];
+        for(int i = 0; i < s1.length(); i ++){
+            letter[s1.charAt(i) - 'a'] ++;
+            letter[s2.charAt(i) - 'a'] --;
+        }
+        for(int i = 0; i < letter.length; i ++)
+            if(letter[i] != 0)
+                return false;
+
+        for(int i = 1; i < s1.length(); i ++){
+            if(isScramble(s1.substring(0, i), s2.substring(0, i)) && isScramble(s1.substring(i), s2.substring(i)))
+                return true;
+            if(isScramble(s1.substring(0, i), s2.substring(s2.length() - i)) && isScramble(s1.substring(i), s2.substring(0, s2.length() - i)))
+                return true;
+        }
+
+        return false;
+    }
+}
+
+public class IsScramble {
+    public static void main(String[] args){
+        String s1 = "abcde";
+        String s2 = "caebd";
+        System.out.println(new Solution().isScramble(s1, s2));
+    }
+}
