80f322cb914523a3e15f8d291d4be1b1814fc505
Ruby <gaojr0110@sina.com>
2018-09-06 15:25:20 +0800

    116

--- src/connect/Connect.java
+++ src/connect/Connect.java
@@ -1,6 +1,17 @@
 package connect;
+/*leetcode 116
+Given a binary tree
 
-import jdk.nashorn.api.tree.Tree;
+struct TreeLinkNode {
+  TreeLinkNode *left;
+  TreeLinkNode *right;
+  TreeLinkNode *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+*/
+
 
 class TreeLinkNode {
     int val;
